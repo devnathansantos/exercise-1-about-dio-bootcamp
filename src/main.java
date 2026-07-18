76e0e4e9ce5400edb5bff81c83d3f0fe9796cbe8
devnathansantos <nathandossantosbraga1@gmail.com>
2026-07-18 12:30:22 -0300

    project finish

--- src/main.java
+++ src/main.java
@@ -1,2 +1,22 @@
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class main {
+    public static void main (String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Digite o ano do seu nascimento: ");
+        int yearOfBorn = scanner.nextInt();
+
+        LocalDate atualData = LocalDate.now();
+        int atualDataYear = atualData.getYear();
+
+        int age = atualDataYear - yearOfBorn;
+
+        System.out.printf("Ola %s você tem %s anos.%n", name, age);
+        scanner.close();
+    }
 }
